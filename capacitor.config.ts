82c1d9089176159137be41b94e92a090e6e2cdd5
Safyan Ahmad <safyanahmad327@gmail.com>
2026-07-12 05:07:41 -0700

    Fix GitHub Actions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.safyan.alnoor',
   appName: 'AL Noor',
-  webDir: 'dist'
+  webDir: '.output/public'
 };
 
 export default config;
